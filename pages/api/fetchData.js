7f6a78b7ba98b76711bd125aba7fcacdaaadb02b
JDIKWKK3223 <3234445@protonmail.com>
2024-11-30 02:11:12 -0600

    Update fetchData.js

--- pages/api/fetchData.js
+++ pages/api/fetchData.js
@@ -1,1 +1,12 @@
 
+// pages/api/fetchData.js
+import { getData } from '../../lib/getData';
+
+export default async function handler(req, res) {
+  try {
+    const data = await getData();
+    res.status(200).json(data);  // Return the data as JSON
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch data' });
+  }
+}
